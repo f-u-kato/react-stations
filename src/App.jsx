02c2bb813b9e60e32a6e7f074638e3f0e500b4d2
roido-name <roido4473@gmail.com>
2023-09-12 09:58:46 +0900

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 // DO NOT DELETE
 
 import './App.css'
+import React, { useState } from 'react';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/waterdog-spanish/20180714_201544.jpg")
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h2>Dogアプリ</h2>
+      </header>
+      <h2>犬のガオウを表示するサイトです</h2>
+      <img src={dogUrl} /> <br></br>
+      <button onClick={() => { 
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setDogUrl(result.message)
+          }) 
+        }}>
+        更新
+      </button>
     </div>
+
   )
 }
